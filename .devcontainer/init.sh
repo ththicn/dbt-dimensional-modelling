b7d1f414819615514d1a30077fe6ba5d66da09a5
ththicn <ththicn@gmail.com>
2025-01-13 10:59:16 +0900

    refactor: update project and profiles directory names to adventure_works for consistency across configuration files

--- .devcontainer/init.sh
+++ .devcontainer/init.sh
@@ -5,6 +5,7 @@ gcloud config set project "ththicn-dev"
 # Execute gcloud authentication if credentials file does not exists
 if [ ! -f /root/.config/gcloud/application_default_credentials.json ]; then
     gcloud auth application-default login
+    gcloud auth application-default set-quota-project "ththicn-dev"
 fi
 
 dbt deps --profiles-dir /workspaces/dbt-dimensional-modelling/adventure_works \
