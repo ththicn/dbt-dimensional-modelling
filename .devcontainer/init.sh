f09e8a2a2a2f19c9ed254f0391823612fb301bfb
ththicn <ththicn@gmail.com>
2025-01-13 10:42:39 +0900

    fix: update dbt dependencies command to use correct adventure_works directory

--- .devcontainer/init.sh
+++ .devcontainer/init.sh
@@ -7,5 +7,5 @@ if [ ! -f /root/.config/gcloud/application_default_credentials.json ]; then
     gcloud auth application-default login
 fi
 
-dbt deps --profiles-dir /workspaces/dbt-dimensional-modelling/adventureworks \
-    --project-dir /workspaces/dbt-dimensional-modelling/adventureworks
+dbt deps --profiles-dir /workspaces/dbt-dimensional-modelling/adventure_works \
+    --project-dir /workspaces/dbt-dimensional-modelling/adventure_works
